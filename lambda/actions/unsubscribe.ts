bfa0de2e7dd7e01861242abdb93b877547088958
Vladimir Feskov <v@vladimirfeskov.com>
2017-05-21 20:53:02 +0200

    fix(lambda): fix rsa private key preparation

--- lambda/actions/unsubscribe.ts
+++ lambda/actions/unsubscribe.ts
@@ -1,5 +1,5 @@
 import * as JWT from 'jsonwebtoken';
-const JWT_RSA_PRIVATE_KEY = process.env.JWT_RSA_PRIVATE_KEY.replace(/\\n/, '\n');
+const JWT_RSA_PRIVATE_KEY = process.env.JWT_RSA_PRIVATE_KEY.replace(/\\n/g, '\n');
 const { APP_URL } = process.env;
 
 export function getUnsubscribeUrl(username: string, target: string) {
